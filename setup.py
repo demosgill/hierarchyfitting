8710ffee3a00a4120abcada60f94d575a7592c9f
gillDemosRemald <gdemosg@gmail.com>
2016-11-22 11:14:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hierarchyfitting',
+    version='',
+    packages=[''],
+    url='',
+    license='ETHZ',
+    author='gdemos',
+    author_email='',
+    description=''
+)
